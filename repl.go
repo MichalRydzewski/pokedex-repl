11dee96840c148fde2df63c31c1d7c9199278e1e
Michal <michal.rydzewski.03@gmail.com>
2026-01-09 16:18:14 +0100

    initial working repl

--- repl.go
+++ repl.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func CleanInput(text string) []string {
 	sliceOfWords := strings.Fields(text)
-
 	return sliceOfWords
 }
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		word := strings.ToLower(scanner.Text())
+		fmt.Printf("Your command was: %v\n", word)
+	}
 }
